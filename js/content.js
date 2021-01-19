279b680672f1483ba9a79bea10bf8071c209fb23
asabuncuoglu13 <asabuncuoglu13@ku.edu.tr>
2021-01-19 10:11:43 +0300

    youtube-add-on bug fixed

--- js/content.js
+++ js/content.js
@@ -6,21 +6,6 @@ var _emptyPopUp = {
   detailButton: "",
 };
 
-var _baitAdsPopUp = {
-  question: "",
-  title: "Most Viewed Videos in Your Area (Istanbul)",
-  explanation: "Based on your internet provider (Turk Telekom) info, we know that you are in Istanbul. According to our data scientists, you will be 80% likely to watch these recommended videos similar to your neighbours. this is why you are viewing this content now.",
-  style:"bait-ads",
-  detailButton: "./detail/clickbait.html",
-};
-
-var _baitContentPopUp = {
-  question: "Is it a clickbait content?",
-  title: "The Thumbnail Images can be Misleading",
-  explanation: "It is called “clickbait.” The content creator tries to grab your attention with misleading images and content.",
-  style:"bait-content",
-  detailButton: "./detail/clickbait.html",
-};
 
 var _locationPopUp = {
   question: "How do I see this content?",
@@ -38,28 +23,22 @@ var _recommendationPopUp = {
   detailButton: "./detail/recommendation.html",
 };
 
-var _passwordSecondPopUp = {
-  question: "Is my password, okay?",
-  title: "A Hacker can Crack This Password in Seconds",
-  explanation: "This password is one of the most commonly used passwords. If hackers intend to hack your account, this is one of the first passwords they will try.",
-  style:"password-second",
-  detailButton: "./detail/password.html",
+
+var _securePasswordPopUp = {
+  question: "Can strong password protect my loved ones?",
+  title: "Give Time to Create a Real Secure Password",
+  explanation: "You might think that your data is not important but from your profile you put your friends and family in danger. Someone might behave like you in an online environment and your close ones can believe this attack!",
+  style:"pass-secure",
+  detailButton: "./detail/secure.html",
 };
 
-var _passwordDayPopUp = {
-  question: "Is my password, okay?",
-  title: "A Hacker needs 2-3 days to crack this password",
-  explanation: "This password is not easy to crack. After the occurrence of a potentıal securıty breach, you may have the chance to change your password.",
-  style:"password-day",
-  detailButton: "./detail/password.html",
-};
 
-var _passwordMonthPopUp = {
-  question: "Is my password, okay?",
-  title: "A Hacker needs months to crack this password",
-  explanation: "This password consists of more than 8 characters, including numbers and symbols. It is not easy to crack ıt.",
-  style:"password-month",
-  detailButton: "./detail/password.html",
+var _twoFactorPopUp = {
+  question: "Should I use two factor authentication?",
+  title: "Use Two-Factor Authentication If It is Available",
+  explanation: "It makes you more secure. The application will send a code to your personal smartphone to approve if it is really you!",
+  style:"pass-twofactor",
+  detailButton: "./detail/secure.html",
 };
 
 var uncheckedModes = {
@@ -82,14 +61,11 @@ function changeValue(message) {
 
 // Youtube
 var addNudgeButtons = () => {
-  // bindPopUpBeforeElement(document.getElementById("contents"), _passwordMonthPopUp);
-  // bindPopUpBeforeElement(document.getElementById("contents"), _passwordDayPopUp);
-  // bindPopUpBeforeElement(document.getElementById("contents"), _passwordSecondPopUp);
   if (getLoginStatus()){
     bindPopUpBeforeElement(document.getElementById("contents"), _recommendationPopUp);
+    bindPopUpBeforeElement(document.getElementById("avatar-btn"), _securePasswordPopUp);
   } else {
     bindPopUpBeforeElement(document.getElementById("contents"), _locationPopUp);
+    bindPopUpBeforeElement(document.getElementById("end"), _twoFactorPopUp);
   }
-  bindPopUpInsideElement(document.getElementById("upnext"), _baitContentPopUp);
-  //bindPopUpBeforeElement(document.getElementById("secondary"), _baitAdsPopUp);
 };
